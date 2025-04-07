45545fc3229ea1f508a978bc0c127f0cacf2e527
Srini <srven@microsoft.com>
2025-04-06 21:12:38 -0700

    Add ListNode class

---
+++ Prep/Prep/ListNode.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+public class ListNode
+{
+    public int Value { get; set; }
+    public ListNode Next { get; set; }
+
+    public ListNode(int value = 0, ListNode next = null)
+    {
+        Value = value;
+        Next = next;
+    }
+}
+
+public class List
+{
+    private ListNode head;
+
+    public void Add(int value)
+    {
+        if (head == null)
+        {
+            head = new ListNode(value);
+        }
+        else
+        {
+            ListNode current = head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = new ListNode(value);
+        }
+    }
+
+    public void Print()
+    {
+        if (head != null)
+        {
+            ListNode current = this.head;
+            while (current != null)
+            {
+                Console.Write(current.Value + "->");
+                current = current.Next;
+            }
+        }
+        else
+        {
+            Console.WriteLine("List is empty.");
+        }
+    }
+
+    // Leet code #83. Remove Duplicates from Sorted List
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+
+        if (head == null)
+        {
+            return null;
+        }
+
+        ListNode curr = head;
+        ListNode temp = curr.next;
+
+        while (temp != null)
+        {
+            if (temp.val == curr.val)
+            {
+                temp = temp.next;
+            }
+            else
+            {
+                curr.next = temp;
+                curr = temp;
+
+                temp = temp.next;
+            }
+        }
+
+        curr.next = temp;
+
+        return head;
+    }
+}
--- Prep/Prep/Program.cs
+++ Prep/Prep/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List list = new List();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+
+        Console.WriteLine("The elements in the list are:");
+        list.Print();
+
+        Console.WriteLine();
+    }
+}
