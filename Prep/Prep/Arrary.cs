47e3fc4b364bdbd0e4f0f34a6a1fc061a212f0d4
Srini <srven@microsoft.com>
2025-04-26 21:54:48 -0700

    Create Arrary.cs

---
+++ Prep/Prep/Arrary.cs
@@ -0,0 +1,43 @@
+﻿public class Array
+{
+    // 733: https://leetcode.com/problems/flood-fill/description/
+    public static int[][] FloodFill(int[][] image, int sr, int sc, int color)
+    {
+        return FloodFillHelper(image, sr, sc, color, image[sr][sc]);
+    }
+
+    private static int[][] FloodFillHelper(int[][] image, int sr, int sc, int newColor, int oldColor)
+    {
+        if (image[sr][sc] == newColor)
+        {
+            return image;
+        }
+
+        if (image[sr][sc] == oldColor)
+        {
+            image[sr][sc] = newColor;
+        }
+
+        if (sr + 1 < image.Length && image[sr + 1][sc] == oldColor)
+        {
+            FloodFillHelper(image, sr + 1, sc, newColor, oldColor);
+        }
+
+        if (sr - 1 >= 0 && image[sr - 1][sc] == oldColor)
+        {
+            FloodFillHelper(image, sr - 1, sc, newColor, oldColor);
+        }
+
+        if (sc + 1 < image[0].Length && image[sr][sc + 1] == oldColor)
+        {
+            FloodFillHelper(image, sr, sc + 1, newColor, oldColor);
+        }
+
+        if (sc - 1 >= 0 && image[sr][sc - 1] == oldColor)
+        {
+            FloodFillHelper(image, sr, sc - 1, newColor, oldColor);
+        }
+
+        return image;
+    }
+}
