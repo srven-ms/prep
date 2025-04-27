5c96eff2cc3407a34f17ab28c35236649e2ef228
Srini <srven@microsoft.com>
2025-04-26 17:38:08 -0700

    Create TreeNode.cs

---
+++ Prep/Prep/TreeNode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class TreeNode
+{
+    public int val { get; set; }
+    public TreeNode left { get; set; }
+    public TreeNode right { get; set; }
+
+    public TreeNode(int value = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = value;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Tree
+{
+    public TreeNode root { get; set; }
+
+    public Tree()
+    {
+        root = null;
+    }
+
+    public void Inorder(TreeNode node)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        Inorder(node.left);
+        Console.Write(node.val + "->");
+        Inorder(node.right);
+    }
+}
